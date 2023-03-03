0a0bcfeb862f7be5a32937905ca5e47a97e4d8e2
Md Tanjil Ahmed <46747856+tanjil14@users.noreply.github.com>
2023-03-03 23:53:17 +0600

    create about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,19 @@ const About = () => {
     <>
       <Navbar />
       <main className="px-8 md:px-16">
+        <section>
+          <div className="w-full md:w-2/5">
+            <h1 className="text-Black font-bold text-2xl md:text-5xl my-8">About Finsweet Podcast</h1>
+            <p className="text-justify mb-8 text-base font-normal text-Black">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+            <button className="bg-Blue cursor-pointer py-2 px-4 text-[#F4F2FF] text-base font-medium">
+              Subscribe Now!
+            </button>
+          </div>
+        </section>
         <Sponsor />
       </main>
       <Footer />
